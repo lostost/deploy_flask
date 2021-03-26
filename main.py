9714bab8dfdc89ef18f3b9058ec8c81cb8340721
lostost <iamvadya@yandex.ru>
2021-03-26 14:50:08 +0300

    flask application

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask!!!!!!!!!"
+    return "5Привет от приложения Flask!!!!!!!!!"
 
 
 if __name__ == '__main__':
